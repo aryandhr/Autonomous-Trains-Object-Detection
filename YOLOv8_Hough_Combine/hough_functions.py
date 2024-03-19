aec3e86f63a46f598f43756f436f41d7e19e4a40
matteo <robe1157@msu.edu>
2024-03-19 14:09:54 -0400

    Added cropping function

--- YOLOv8_Hough_Combine/hough_functions.py
+++ YOLOv8_Hough_Combine/hough_functions.py
@@ -21,8 +21,11 @@ class LineDetector:
 #         #cv2.destroyAllWindows()
 #         cv2.imwrite('hough_output_image.jpg', processed_image)
 
-    def detect_lines_image(self, image_path):
-        image = cv2.imread(image_path)
+    def detect_lines_image(self, image_path, path=True):
+        if path is True:
+            image = cv2.imread(image_path)
+        else:
+            image = image_path
         if image is None:
             print("Error opening image")
             return
@@ -66,7 +69,7 @@ class LineDetector:
     def process_frame(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 50, 150)
-        lines = cv2.HoughLinesP(edges, 1, math.pi / 180, 100, minLineLength=200, maxLineGap=5)
+        lines = cv2.HoughLinesP(edges, 1, math.pi / 180, 100, minLineLength=200, maxLineGap=10)
         if lines is not None:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
